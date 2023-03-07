5dffd60b61f2d0469750e58ad22b35871ee80d74
Ammar ALArashi <ammaralarashi.engineer@gmail.com>
2023-03-07 14:26:57 +0300

    this is sample youtubedownloader

---
+++ download.py
@@ -0,0 +1,9 @@
+from pytube import YouTube
+
+url = input('what is the url? ')
+yt = YouTube(url=url)
+print
+d  = yt.streams.get_by_resolution(resolution='720p')
+print(f'{d.title} downloading .....')
+d.download()
+print('finished downloading !!!!')
